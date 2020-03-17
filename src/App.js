a9791ddb706eaba327b25603887352e9d30aa109
WEB DEVELOPER <buka4rill@gmail.com>
2020-03-17 12:52:01 +0100

    a whole new work

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+import './App.css';
+
+
+
+class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+
+    console.log(res.data);
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
